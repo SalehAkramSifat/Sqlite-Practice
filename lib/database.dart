7a66199e10a3155ca7bfb3d56edb42b18c5c99de
Saleh Akram Sifat <salehakramsifat@gmail.com>
2025-03-08 23:24:10 +0600

    Initial Commit

---
+++ lib/database.dart
@@ -0,0 +1,58 @@
+
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB();
+    return _database!;
+  }
+
+  Future<Database> _initDB() async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, 'notes_database.db');
+    print('Database Path: $path');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute(''' 
+          CREATE TABLE notes(
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            content TEXT
+          )
+        ''');
+      },
+    );
+  }
+
+  // Insert Note
+  Future<void> insertNote(String content) async {
+    final db = await database;
+    await db.insert('notes', {'content': content},
+        conflictAlgorithm: ConflictAlgorithm.replace);
+    print('Inserted Note: $content');
+  }
+
+  // Get Notes
+  Future<List<Map<String, dynamic>>> getNotes() async {
+    final db = await database;
+    var result = await db.query('notes');
+    print('Database Notes: $result');
+    return result;
+  }
+
+  // Delete Note
+  Future<void> deleteNote(int id) async {
+    final db = await database;
+    await db.delete('notes', where: "id = ?", whereArgs: [id]);
+    print('Deleted Note with ID: $id');
+  }
+}
